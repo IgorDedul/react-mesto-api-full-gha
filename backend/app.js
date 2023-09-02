4b828f616e7e0922efe57592155b4e193133f582
IgorDedul <dedul_igor@mail.ru>
2023-09-02 16:33:23 +0300

    feat: add CORS

--- backend/app.js
+++ backend/app.js
@@ -27,13 +27,13 @@ const limiter = rateLimit({
   // Ограничевает обращения до 100 за 15 минут.
 });
 
-app.use(cors());
 app.use(helmet());
 app.use(requestLogger);
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
